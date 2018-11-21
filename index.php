489826ef4d076c069d0137ab456432527620d2ce
Mogammad Taariq Bardien <bardientaariq11@gmail.com>
2018-11-21 15:26:34 +0200

    Finished display and added buttons and shows results

--- index.php
+++ index.php
@@ -5,145 +5,177 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Anime Quiz</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<form action="upload.php" method="post">
+    <?php
+        $break = "<br>";
 
-<?php
-    $break = "<br>";
+        $questions = array ('1. Who is the main character in One Piece?',
+            '2. Who the sidekick of Saitama in One Punch Man?',
+            '3. From which series is the Shadow Realm used?', 
+            '4. What was Kurogane Ikki know as in Rakudai Kishi no Eiyuutan?', 
+            '5. Who is the brother of Edward Elric in Full Metal Alchemist?', 
+            '6. In Soul Eater what does Death The Kid hate the most?', 
+            '7. Which part of his powers did Ichigo get from his mother?', 
+            '8. How many races exist in Disboard, the fantasy world of No Game No Life?', 
+            '9. What are the weapons in Akame ga Kill called?', 
+            '10. What is the constitution of Rikuo Nura in Nurarihyon no Mago?', 
+            '11. What kind of mage is Lucy from Fairy Tail?', 
+            '12. Which animal is associated with Itachi from Naruto?', 
+            '13. What is the name of the pharoah that is in the Millennium Puzzle from Yu-Gi-Oh?', 
+            '14. Who is the blue haired maid from Re:Zero?', 
+            '15. What is Yato from Noragami?', 
+            '16. Who is known as Mr.0 in One Piece?', 
+            '17. How did Luffy obtain his powers?', 
+            '18. What is the curse Saitama from One Punch Man has?', 
+            '19. When Baby Beelzebub cries what happens?', 
+            '20. How old is Meliodas from Seven Deadly Sins?');
+        
+        $answerOne = array('Sanji', 
+            'L', 
+            'Yu-Gi-Oh', 
+            'Failed Knight', 
+            'Gintoki', 
+            'Unsymettrical things', 
+            'Vizard', '14', 
+            'Imperial Arms', 
+            '1/2 Human, 1/2 Demon', 
+            'Spirit Mage', 'Eagle', 
+            'Atem', 
+            'Ram', 
+            'A God', 
+            'Doflamingo', 
+            'Miracle', 
+            'He is cannot grow hair', 
+            'Shocking Occurs', 
+            '3000');
 
-    $question = array ('Who is the main character in One Piece?',
-        'Who the sidekick of Saitama in One Punch Man?',
-        'From which series is the Shadow Realm used?', 
-        'What was Kurogane Ikki know as in Rakudai Kishi no Eiyuutan?', 
-        'Who is the brother of Edward Elric in Full Metal Alchemist?', 
-        'In Soul Eater what does Death The Kid hate the most?', 
-        'Which part of his powers did Ichigo get from his mother?', 
-        'How many races exist in Disboard, the fantasy world of No Game No Life?', 
-        'What are the weapons in Akame ga Kill called?', 
-        'What is the constitution of Rikuo Nura in Nurarihyon no Mago?', 
-        'What kind of mage is Lucy from Fairy Tail?', 
-        'Which animal is associated with Itachi from Naruto?', 
-        'What is the name of the pharoah that is in the Millennium Puzzle from Yu-Gi-Oh?', 
-        'Who is the blue haired maid from Re:Zero?', 
-        'What is Yato from Noragami?', 
-        'Who is known as Mr.0 in One Piece?', 
-        'How did Luffy obtain his powers?', 
-        'What is the curse Saitama from One Punch Man has?', 
-        'When Baby Beelzebub cries what happens?', 
-        'How old is Meliodas from Seven Deadly Sins?');
+        $answerTwo = array('Zoro', 
+            'Genos', 
+            'Duel Masters', 
+            'Best Knight', 
+            'Renji', 
+            'Doing work', 
+            'Hollow', 
+            '15', 
+            'Imperial Weapons', 
+            '1/4 Human, 3/4 Demon', 
+            'Celestial Spirit Mage', 
+            'Snake', 
+            'Yami Yugi', 
+            'Rem', 
+            'A Spirit', 
+            'Ivan', 
+            'Devil Fruit', 
+            'He is mentally slow', 
+            'Floods Occur', 
+            '28');
+
+        $answerThree = array('Luffy', 
+            'Gon', 
+            'One Piece', 
+            'Worst One', 
+            'Alphonse', 
+            'Black Star', 
+            'Shinigami', 
+            '16', 
+            'Imperial Killers', 
+            '3/4 Human, 1/4 Demon', 
+            'Celestial Mage', 
+            'Hawk', 
+            'Marik', 
+            'Emilia', 
+            'An Angel', 
+            'Crocodile', 
+            'Devil Eggs', 
+            'He is always alone', 
+            'Things shatter from the high pitch', 
+            '250');
+
+        $answerFour = array('Chopper', 
+            'Kirito', 
+            'Bleach', 
+            'Best One', 
+            'Sora', 
+            'Maka Albern', 
+            'Quincy', 
+            '17', 
+            'Imperial Armaments', 
+            '2/3 Human, 1/3 Demon', 
+            'Key Mage', 
+            'Crow', 
+            'Bakura', 
+            'Luna', 
+            'A Devil', 
+            'Akainu', 
+            'Magical Item', 
+            'He always wins in one punch', 
+            'Demons are summoned', 
+            '90');
+
+        $answers = array(3, 
+            2, 
+            1, 
+            3, 
+            3, 
+            1, 
+            4, 
+            3, 
+            1, 
+            3, 
+            2, 
+            4, 
+            1, 
+            2, 
+            1, 
+            3, 
+            2, 
+            4, 
+            1, 
+            1);
+
+        
+
+        for ($a = 0 ; $a <= 19; $a++) {
+            $fullQuestion[$a] = array($questions[$a] ,$answerOne[$a],$answerTwo[$a],$answerThree[$a],$answerFour[$a], $answers[$a]);
+        }
+
+        // var_dump($fullQuestion);
+    ?>
     
-    $answerOne = array('Sanji', 
-        'L', 
-        'Yu-Gi-Oh', 
-        'Failed Knight', 
-        'Gintoki', 
-        'Unsymettrical things', 
-        'Vizard', '14', 
-        'Imperial Arms', 
-        '1/2 Human, 1/2 Demon', 
-        'Spirit Mage', 'Eagle', 
-        'Atem', 
-        'Ram', 
-        'A God', 
-        'Doflamingo', 
-        'Miracle', 
-        'He is cannot grow hair', 
-        'People shock', 
-        '3000');
+    <?php
+	for ($i = 0; $i < count($fullQuestion); $i++) { ?>
 
-    $answerTwo = array('Zoro', 
-        'Genos', 
-        'Duel Masters', 
-        'Best Knight', 
-        'Renji', 
-        'Doing work', 
-        'Hollow', 
-        '15', 
-        'Imperial Weapons', 
-        '1/4 Human, 3/4 Demon', 
-        'Celestial Spirit Mage', 
-        'Snake', 
-        'Yami Yugi', 
-        'Rem', 
-        'A Spirit', 
-        'Ivan', 
-        'Devil Fruit', 
-        'He is mentally slow', 
-        'Floods occur', 
-        '28');
+	<section class='container'>
 
-    $answerThree = array('Luffy', 
-        'Gon', 
-        'One Piece', 
-        'Worst One', 
-        'Alphonse', 
-        'Black Star', 
-        'Shinigami', 
-        '16', 
-        'Imperial Killers', 
-        '3/4 Human, 1/4 Demon', 
-        'Celestial Mage', 
-        'Hawk', 
-        'Marik', 
-        'Emilia', 
-        'An Angel', 
-        'Crocodile', 
-        'Devil Eggs', 
-        'He is always alone', 
-        'Things shatter from the high pitch', 
-        '250');
-
-    $answerFour = array('Chopper', 
-        'Kirito', 
-        'Bleach', 
-        'Best One', 
-        'Sora', 
-        'Maka Albern', 
-        'Quincy', 
-        '17', 
-        'Imperial Armaments', 
-        '2/3 Human, 1/3 Demon', 
-        'Key Mage', 
-        'Crow', 
-        'Bakura', 
-        'Luna', 
-        'A Devil', 
-        'Akainu', 
-        'Magical Item', 
-        'He always wins in one punch', 
-        'Demons are summoned', 
-        '90');
-
-    $answers = array('3', 
-        '2', 
-        '1', 
-        '3', 
-        '3', 
-        '1', 
-        '4', 
-        '3', 
-        '1', 
-        '3', 
-        '2', 
-        '4', 
-        '1', 
-        '2', 
-        '1', 
-        '3', 
-        '2', 
-        '4', 
-        '1', 
-        '1');
-
-     
-
-    for ($a = 0 ; $a <= 19; $a++) {
-        $fullQuestion[$a] = array($question[$a] ,$answerOne[$a],$answerTwo[$a],$answerThree[$a],$answerFour[$a], $answers[$a]);
-    }
-
-    var_dump($fullQuestion);
-
+			<h3>
+				<?php echo $fullQuestion[$i][0]; ?>
+			</h3>
+			<section>
+			<?php 
+                $a = 1;
+			for ($b = $i*4; $b < ($i+1)*4; $b++) { 
+				?>
+				<input type='radio' name="<?php echo $i; ?>" 
+                value="<?php echo $a ?>" 
+                id="<?php echo $b ?>">
+                <label class="option"><?php echo $fullQuestion[$i][$a] ?>
+                </label><br>
+			<?php
+                    $a++; 
+		} ?>
+			</section>
+		</section>
+        
+	<?php }
 ?>
+        	<section class="submit">
+                <br>
+			    <input type="submit" value="Submit!">
+	        </section>
+    </form>
     
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -4,9 +4,70 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Recieve information</title>
+    <title>Recieve information and Display</title>
 </head>
 <body>
+
+<?php
+
+        $answers = array( '3', 
+        '2', 
+        '1', 
+        '3', 
+        '3', 
+        '1', 
+        '4', 
+        '3', 
+        '1', 
+        '3', 
+        '2', 
+        '4', 
+        '1', 
+        '2', 
+        '1', 
+        '3', 
+        '2', 
+        '4', 
+        '1', 
+        '1');
+
+        $total = 0;
+        for ($e=0; $e <= 19 ; $e++) { 
+            if ($_POST[$e] == $answers[$e] ){
+                $total++;
+            }  
+        }
+        // echo $total."/20";
+        switch ($total) {
+            case ($total < 10):
+                    echo "You have much to learn<br>";
+                    echo $total."/20";
+                break;
+            case ($total > 9 && $total < 16):
+                    echo "You can still do better<br>";
+                    echo $total."/20";
+                break;
+            case ($total > 15 ):
+                    echo " You have done well<br>";
+                    echo $total."/20";
+                break;
+
+            default:
+                    echo "This is not working right<br>";
+                    echo $total."/20"; 
+                break;
+        }
+
+            
+
+?>
+
+	<section class="submit">
+		<form action="index.php" method="post">
+		<input type="submit" value="Redo">
+		
+		</form>
+	</section>
     
 </body>
 </html>
